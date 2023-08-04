2879e2b425394b1ded81eb03fc21a2c22d6f4ff5
DESKTOP-VS26VVU\vijay <vijayaratnamythanushan000@gmail.com>
2023-08-04 17:04:36 +0530

    server connection and implementation

--- server/index.js
+++ server/index.js
@@ -1,86 +1,27 @@
-// const MovieModel = require("./database/movies");
-// const UserModel = require("./database/users");
-require('dotenv').config()
-const express = require("express");
-var cors = require('cors')
-const helmet = require("helmet");
-var mongoose = require("mongoose");
+// import { UserModel, ImageModel, ReviewModel } from './database/allModels';
 require("@babel/core").transform("code", {
     presets: ["@babel/preset-env"],
 });
+require('dotenv').config()
+import express  from "express";
+import cors from "cors";
+import helmet from "helmet"
+// var mongoose = require("mongoose");
+
+//connection
+import ConnectDB from "./database/connection";
+
 const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(helmet());
 
-
-
-const mongoDB = process.env.mongoDB_URI;
-mongoose.connect(mongoDB, {useNewUrlParser:true, useUnifiedTopology:true}).then(()=>console.log("CONNECTION ESTABLISHED"));
-
-
-//http://localhost:3000/
-app.get("/",(req,res)=>{
-    return res.json({"WELCOME": 'to my backend for the ZOMATO-MASTER'});
-});
-
-/*
-route      -------/movies
-description ------get all movies
-access  ----------public
-parameter---------none
-methods-----------GET
-*/
-
-//http://localhost:5000/movies
-app.get("/movies",async(req, res) => {//set request and response and set url
-    const getAllMovies= await MovieModel.find();
-    return res.json(getAllMovies);
-});
-
-
-
-
-/*
-route      -------/movies/:id
-description ------get a one  movie
-access  ----------public
-parameter---------none
-methods-----------GET
-*/
-
-//http://localhost:3000/movie/:id
-app.get("/movie/:id",async(req, res) => {
-    const {id} = req.params;
-    const getMovie= await MovieModel.findOne({_id: id});
-    return res.json(getMovie);
-});
-
-
-
-
-/*
-route      -------/user-register
-description ------post single user details in user collection
-access  ----------public
-parameter---------none
-methods-----------POST
-*/
-
-//http://localhost:3000/user-register
-app.post("/user-register",async(req, res) => {//set request and response and set url
-    const addNewUser = await UserModel.create(req.body);
-    return res.json({userAdded: addNewUser, message:"User was added!!!"});
-   
-});
-
-
-
-
-
-
-
-
-app.listen(5000,()=>{
-    console.log("MY EXPRESS APP IS RUNNING.....");
-});
+app.listen(4000, ()=>{
+    ConnectDB()
+    .then(()=>{
+        console.log("Zomato Server is Running...!!")
+    }).catch((error)=>{
+        console.log("Zomato Server is Running.. But DB Connection is Failed....!!!");
+        console.log(error);
+    })
+})
